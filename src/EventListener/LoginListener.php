ebcc44eb87120a88a78ea8444882a8ad033ccabe
valentin <valentin@roliste-chaotique.be>
2022-10-29 09:53:44 +0200

    notification delete after 3months

--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -2,6 +2,7 @@
 
 namespace App\EventListener;
 
+use App\Entity\Notification\Notification;
 use App\Service\BadgeManager;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
@@ -31,6 +32,18 @@ class LoginListener
         }
         if ('12-17' <= $holiday->format('m-d') && $holiday->format('m-d') <= '12-31') {
             $this->badgeManager->checkAndUnlock($user, 'christmas', 1);
+        }
+
+        $notifications = $this->entityManager->getRepository(Notification::class)
+            ->findBy(['user' => $user]);
+
+        if ($notifications) {
+            $date = new DateTime('-3 months');
+            foreach ($notifications as $notification) {
+                if ($notification->getCreatedAt()->format('Y-m-d') < $date->format('Y-m-d')) {
+                    $this->entityManager->remove($notification);
+                }
+            }
         }
 
         // Persist the data to database.
