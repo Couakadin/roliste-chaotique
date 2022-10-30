c779be4ac5bb0f68e7ddec1764b0682ee5626873
valentin <valentin@roliste-chaotique.be>
2022-10-30 16:03:49 +0100

    datetimeImmutable sur migrations

--- migrations/Version20220804132234.php
+++ migrations/Version20220804132234.php
@@ -20,7 +20,7 @@ final class Version20220804132234 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE rc_token (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, token VARCHAR(52) DEFAULT NULL, type VARCHAR(25) NOT NULL, expired_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_55C4F3F85F37A13B (token), INDEX IDX_55C4F3F8A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE rc_token (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, token VARCHAR(52) DEFAULT NULL, type VARCHAR(25) NOT NULL, expired_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_55C4F3F85F37A13B (token), INDEX IDX_55C4F3F8A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE rc_token ADD CONSTRAINT FK_55C4F3F8A76ED395 FOREIGN KEY (user_id) REFERENCES rc_user (id)');
     }
 
--- migrations/Version20220804140244.php
+++ migrations/Version20220804140244.php
@@ -20,7 +20,7 @@ final class Version20220804140244 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE rc_event (id INT AUTO_INCREMENT NOT NULL, table_id INT NOT NULL, name VARCHAR(255) NOT NULL, slug VARCHAR(128) NOT NULL, type VARCHAR(15) NOT NULL, start DATETIME NOT NULL, end DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_315D5864ECFF285C (table_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE rc_event (id INT AUTO_INCREMENT NOT NULL, table_id INT NOT NULL, name VARCHAR(255) NOT NULL, slug VARCHAR(128) NOT NULL, type VARCHAR(15) NOT NULL, start DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', end DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_315D5864ECFF285C (table_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE rc_event ADD content LONGTEXT DEFAULT NULL');
         $this->addSql('ALTER TABLE rc_event ADD CONSTRAINT FK_315D5864ECFF285C FOREIGN KEY (table_id) REFERENCES rc_table (id)');
         $this->addSql('ALTER TABLE rc_event ADD master_id INT DEFAULT NULL');
